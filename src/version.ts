b6c52b4834be9ee1fc9dd293464504dbde41ba67
Bob Matsuoka <bob@matsuoka.com>
2025-12-16 19:26:33 -0500

    chore: pre-release build number increment

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 // This file is auto-generated during build from package.json
 // Do not edit manually - changes will be overwritten
 export const VERSION = '4.5.0';
-export const BUILD_NUMBER = 6;
-export const VERSION_WITH_BUILD = '4.5.0 (build 6)';
+export const BUILD_NUMBER = 7;
+export const VERSION_WITH_BUILD = '4.5.0 (build 7)';
